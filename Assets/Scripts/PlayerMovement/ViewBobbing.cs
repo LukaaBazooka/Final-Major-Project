03ea3455a15d1f54631b187bbf44fc6b86a91093
LukaaBazooka <529052@chrome.escg.ac.uk>
2024-03-27 16:45:53 +0000

    now webgl

--- Assets/Scripts/PlayerMovement/ViewBobbing.cs
+++ Assets/Scripts/PlayerMovement/ViewBobbing.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor.PackageManager;
+//using UnityEditor.PackageManager;
 using UnityEngine;
 using static Unity.VisualScripting.Member;
 
