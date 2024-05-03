7fd9415b9d2c9f8a15b7eb33e1b3750a28bd4003
LukaaBazooka <529052@chrome.escg.ac.uk>
2024-05-03 12:05:22 +0100

    sdsasa
    sadfsafdsafd

--- Assets/IntroScene.cs
+++ Assets/IntroScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,11 +10,24 @@ public class IntroScene : MonoBehaviour
     [SerializeField] AudioSource m_AudioSource;
     [SerializeField] AudioSource SecondSource;
     [SerializeField] RawImage Panel;
+    [SerializeField] AudioSource Radio;
 
+    public TMP_Text _tmpProText;
+    string writer;
 
+    [SerializeField] float delayBeforeStart = 0f;
+    [SerializeField] float timeBtwChars = 0.1f;
+    [SerializeField] string leadingChar = "";
+    [SerializeField] bool leadingCharBeforeDelay = false;
     void Start()
     {
+
+
+        writer = _tmpProText.text;
         StartCoroutine(Load());
+        Cursor.lockState = CursorLockMode.Locked;
+
+
     }
 
     // Update is called once per frame
@@ -25,7 +39,13 @@ public class IntroScene : MonoBehaviour
         m_AudioSource.Play();
         yield return new WaitForSeconds(1f);
         LeanTween.alpha(Panel.rectTransform, 0f, 2f).setEaseInOutQuad();
-        yield return new WaitForSeconds(7.5f);
+        yield return new WaitForSeconds(2f);
+
+        Radio.Play();
+        StartCoroutine(WRITE());
+
+
+        yield return new WaitForSeconds(23f);
         SecondSource.Play();
 
         yield return new WaitForSeconds(17f);
@@ -35,4 +55,60 @@ public class IntroScene : MonoBehaviour
         SceneManager.LoadScene("MainScene");
 
     }
+
+    IEnumerator WRITE()
+    {
+        yield return new WaitForSeconds(1f);
+
+        writer = " Dr. Reed,";
+        StartCoroutine("TypeWriterTMP");
+        yield return new WaitForSeconds(1.02f);
+        
+        writer = " This is a priority-level Alpha retrieval mission.";
+        StartCoroutine("TypeWriterTMP");
+
+        yield return new WaitForSeconds(2.8f);
+        writer = " Your objective is to secure all the relevant research data of Haven Research facility.";
+        StartCoroutine("TypeWriterTMP");
+
+        yield return new WaitForSeconds(4.9f);
+        writer = " However exercise extreme caution,";
+        StartCoroutine("TypeWriterTMP");
+        yield return new WaitForSeconds(2.5f);
+        writer = " Intel indicates a possible containment breach may have led to its abandonment.";
+        StartCoroutine("TypeWriterTMP");
+        yield return new WaitForSeconds(4.5f);
+        writer = " Assume containment breach protocols are in effect at all times.";
+        StartCoroutine("TypeWriterTMP");
+        yield return new WaitForSeconds(4.1f);
+        writer = " Your success is paramount doctor. recover the data no matter the cost.";
+        StartCoroutine("TypeWriterTMP");
+        yield return new WaitForSeconds(6f);
+        _tmpProText.enabled= false;
+
+    }
+
+    IEnumerator TypeWriterTMP()
+    {
+        _tmpProText.text = leadingCharBeforeDelay ? leadingChar : "";
+
+        yield return new WaitForSeconds(delayBeforeStart);
+
+        foreach (char c in writer)
+        {
+            if (_tmpProText.text.Length > 0)
+            {
+                _tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+            }
+            _tmpProText.text += c;
+            _tmpProText.text += leadingChar;
+            yield return new WaitForSeconds(timeBtwChars);
+        }
+
+        if (leadingChar != "")
+        {
+            _tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+        }
+    }
+
 }
