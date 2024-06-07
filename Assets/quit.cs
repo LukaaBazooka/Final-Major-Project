033b124c0bbb47e363b0f3d24eb0039010b9f65b
LukaaBazooka <529052@chrome.escg.ac.uk>
2024-06-07 10:39:49 +0100

    quit

---
+++ Assets/quit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    public void quitgame()
+    {
+        Application.Quit();
+        Debug.Log("asda");
+    }
+}
